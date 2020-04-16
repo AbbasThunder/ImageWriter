412310cf6d24b0acc58d47319ddcd6e7319c24ae
AbbasThunder <abbasaljoker@gmail.com>
2020-04-17 02:19:26 +0300

    Initial Commit

---
+++ Form1.Designer.cs
@@ -0,0 +1,125 @@
+﻿namespace ImageWriter
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Message = new System.Windows.Forms.TextBox();
+            this.ImageBox = new System.Windows.Forms.PictureBox();
+            this.CreateButton = new System.Windows.Forms.Button();
+            this.imageWithTextBox = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.ImageBox)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.imageWithTextBox)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // Message
+            // 
+            this.Message.Location = new System.Drawing.Point(244, 287);
+            this.Message.Multiline = true;
+            this.Message.Name = "Message";
+            this.Message.Size = new System.Drawing.Size(319, 104);
+            this.Message.TabIndex = 0;
+            this.Message.Text = "Message goes here";
+            // 
+            // ImageBox
+            // 
+            this.ImageBox.Location = new System.Drawing.Point(12, 52);
+            this.ImageBox.Name = "ImageBox";
+            this.ImageBox.Size = new System.Drawing.Size(385, 220);
+            this.ImageBox.TabIndex = 1;
+            this.ImageBox.TabStop = false;
+            this.ImageBox.Click += new System.EventHandler(this.ImageBox_Click);
+            // 
+            // CreateButton
+            // 
+            this.CreateButton.Location = new System.Drawing.Point(310, 415);
+            this.CreateButton.Name = "CreateButton";
+            this.CreateButton.Size = new System.Drawing.Size(161, 72);
+            this.CreateButton.TabIndex = 2;
+            this.CreateButton.Text = "Insert Message";
+            this.CreateButton.UseVisualStyleBackColor = true;
+            this.CreateButton.Click += new System.EventHandler(this.CreateButton_Click);
+            // 
+            // imageWithTextBox
+            // 
+            this.imageWithTextBox.Location = new System.Drawing.Point(444, 52);
+            this.imageWithTextBox.Name = "imageWithTextBox";
+            this.imageWithTextBox.Size = new System.Drawing.Size(342, 220);
+            this.imageWithTextBox.TabIndex = 3;
+            this.imageWithTextBox.TabStop = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(9, 27);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(100, 13);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Image Without Text";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(441, 27);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(85, 13);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Image With Text";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(845, 517);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.imageWithTextBox);
+            this.Controls.Add(this.CreateButton);
+            this.Controls.Add(this.ImageBox);
+            this.Controls.Add(this.Message);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.ImageBox)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.imageWithTextBox)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox Message;
+        private System.Windows.Forms.PictureBox ImageBox;
+        private System.Windows.Forms.Button CreateButton;
+        private System.Windows.Forms.PictureBox imageWithTextBox;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,74 @@
+﻿using ColorThiefDotNet;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+namespace ImageWriter
+{
+    
+    public partial class Form1 : Form
+    {
+        private string colorHex;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void ImageBox_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog open = new OpenFileDialog();
+            open.Filter = "Image Files(*.jpg; *.jpeg; *.gif; *.bmp; *.png)|*.jpg; *.jpeg; *.gif; *.bmp; *.png";
+            if (open.ShowDialog() == DialogResult.OK)
+                ImageBox.Image = new Bitmap(open.FileName);
+            
+        }
+
+        private void CreateButton_Click(object sender, EventArgs e)
+        {
+            if (ImageBox.Image == null)
+                MessageBox.Show("Select An Image to write your message", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            else
+            {
+                var colorThief = new ColorThief();
+                colorHex = colorThief.GetColor(ImageBox.Image as Bitmap).Color.ToHexString() ;
+                DrawNewImage();
+                MessageBox.Show($"text : {Message.Text}", "Succss", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+            
+        }
+
+
+        private void DrawNewImage()
+        {
+            var bitmap = new Bitmap(ImageBox.Image);
+
+            using (Graphics graphics = Graphics.FromImage(bitmap))
+            {
+                using (Font arialFont = new Font("Arial", 10))
+                {
+                    PointF cords = new PointF(10f,10f);
+                  
+                    graphics.DrawString(Message.Text,arialFont, GetTextColor(),cords);
+                }
+                imageWithTextBox.Image = bitmap;
+            }
+            
+        }
+
+        private Brush GetTextColor()
+        {
+            
+            ColorConverter conv = new ColorConverter();
+            return new SolidBrush((System.Drawing.Color)conv.ConvertFromString(colorHex));
+        }
+
+       
+    }
+
+  
+}
